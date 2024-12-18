2b8a4ce459aca4eba7cfbe7f5479bea45432a23d
AdsonMozart <110190822+AdsonMozart@users.noreply.github.com>
2024-12-18 11:29:06 -0300

    Consertando pequenos erros e bugs

--- estudos/src/lambdas/Predicado.java
+++ estudos/src/lambdas/Predicado.java
@@ -6,7 +6,7 @@ public class Predicado {
 
 	public static void main(String[] args) {
 		
-		Predicate<Produto> isCaro = prod -> (prod.preco - desconto) >= 750;
+		Predicate<Produto> isCaro = prod -> (prod.preco - prod.desconto) >= 750;
 		
 		Produto produto = new Produto("Notebook", 3893.89, 0.15);
 		System.out.println(isCaro.test(produto));
