8dd3ffc734ba1663a7c3c2bfafaaf564b6da8d78
AdsonMozart <110190822+AdsonMozart@users.noreply.github.com>
2024-12-19 17:56:38 -0300

    Concluindo try, catch e finally

--- estudos/src/excecao/ChecadaVsNaoChecada.java
+++ estudos/src/excecao/ChecadaVsNaoChecada.java
@@ -1,4 +1,4 @@
-package excessao;
+package excecao;
 
 public class ChecadaVsNaoChecada {
 
@@ -12,18 +12,20 @@ public class ChecadaVsNaoChecada {
 
         try {
             geraErro2();
-        } catch (Exception e) {
-            new Exception();
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
         }
 
         System.out.println("Fim :)");
     }
 
-    // Não checada ou não verificada
+    // Exceção não checada ou não verificada
     static void geraErro1() {
         throw new RuntimeException("Ocorreu um erro bem legal! #01");
     }
-    static void geraErro2() {
-        new Exception("Ocorreu um erro bem legal! #02");
+
+    // Exceção checada ou verificada
+    static void geraErro2() throws Exception{
+        throw new Exception("Ocorreu um erro bem legal! #02");
     }
 }
